7f9ce98613972ee58fc7ff63ab2873da3a8b490d
jmxs <inversionesjmxs@gmail.com>
2022-10-13 12:01:16 -0400

    iframe test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,13 @@ const IndexPage = () => (
   <React.Fragment>
     <ThemeProvider theme={theme}>
       <Layout>
+<iframe
+src="https://gateway.ipfscdn.io/ipfs/QmPaVYdGue8zEXFKqrtVHpvzBvufM1DYzw5n1of3KVPG88/nft-drop.html?contract=0x4E17727B7F0F0037b8249d41861dACD153b28313&chainId=137"
+width="600px"
+height="600px"
+style={{maxWidth:"100%"}}
+frameborder="0"
+></iframe>
         <SEO title="Home" />
         <Hero />
         <Services />
